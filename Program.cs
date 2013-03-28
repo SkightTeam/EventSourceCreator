a9d9942f6ff69f3721499b9be64905c609d507da
Hao Wang <HWonner@gmail.com>
2013-03-28 11:36:03 -0600

    Initilze

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EventSourceCreator {
+    class Program {
+        static void Main(string[] args) {
+            if (EventLog.SourceExists(args[0])) {
+                Console.WriteLine("Source already existed");
+            } else {
+                EventLog.CreateEventSource(args[0],"Skight I-Tech");
+                Console.WriteLine("Create success!");
+            }
+        }
+    }
+}
